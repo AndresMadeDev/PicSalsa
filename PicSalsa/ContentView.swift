0012dd571a8368d9b81e7791947e87112a69fa32
AndresMadeDev <andres.made.dev@gmail.com>
2021-05-01 10:27:08 -0400

    Initial Commit

---
+++ PicSalsa/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PicSalsa
+//
+//  Created by Andres Made on 5/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PicSalsa/PicSalsaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicSalsaApp.swift
+//  PicSalsa
+//
+//  Created by Andres Made on 5/1/21.
+//
+
+import SwiftUI
+
+@main
+struct PicSalsaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
